c321cac355692bb208b446d01519752cc79f8700
Maksym Kysil <maksimdestro2000@gmail.com>
2023-02-26 17:26:51 +0200

    adding cors

--- index.js
+++ index.js
@@ -4,8 +4,10 @@
 // const { response } = require('express')
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 
